782e3020bcfa9d8a55628728b5d551d4b5dd6a83
tatsubow <tatsugbrgames@gmail.com>
2025-12-13 09:43:19 +0900

    feat: only_one_loading

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    # Cookieが無ければ初回アクセスと判断
+    @first_visit = cookies[:visited].blank?
+
+    # 初回アクセスならCookieをセット（30日有効）
+    cookies[:visited] = 1 if @first_visit
   end
 end
